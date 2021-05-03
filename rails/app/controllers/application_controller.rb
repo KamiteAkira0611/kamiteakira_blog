5cc2c2b01b76d130e60dc1fd41b48a8fe0db291a
KamiteAkira0611 <kamite.cocorozashi@gmail.com>
2021-05-04 05:30:53 +0900

    save

--- rails/app/controllers/application_controller.rb
+++ rails/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  def index
+    render json: { status: 'SUCCESS', message: "hello kamiteakira blog api" }
+  end
 end
--- rails/config/routes.rb
+++ rails/config/routes.rb
@@ -7,5 +7,7 @@ Rails.application.routes.draw do
   namespace :api do
     resources :posts
   end
+
+  root "application#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
